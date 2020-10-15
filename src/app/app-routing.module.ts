c05bd52911c8779d599e0fe417137391b6924832
crisbennie19 <akowechristian@gmail.com>
2020-10-15 05:05:59 +0100

    employee project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateEmployeeComponent } from './component/create-employee/create-employee.component';
+import { EmployeeDetailComponent } from './component/employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './component/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './component/update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"employees" , component:EmployeeListComponent},
+  {path:"create" , component:CreateEmployeeComponent},
+  {path:"update/:id", component:UpdateEmployeeComponent},
+  {path:"employeeview/:id", component:EmployeeDetailComponent},
+  {path:"", redirectTo:"employees", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './component/employee-list/employee-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+import { CreateEmployeeComponent } from './component/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './component/update-employee/update-employee.component';
+import { EmployeeDetailComponent } from './component/employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
